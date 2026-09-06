c59ea881d4a42607e20dad9375217c5973e6926b
Troy C. <327529+troxor@users.noreply.github.com>
2026-09-05 23:54:49 -0500

    ci: Quick hacks

--- components/umurmur/config.h
+++ components/umurmur/config.h
@@ -4,5 +4,7 @@
 #define USE_MBEDTLS
 /* SSLIMP_VERSION shown in Server_run banner */
 #define SSLIMP_VERSION "mbedTLS-ESP-IDF"
+/* Upstream sets this in CMakeLists (v0.4.1 → "Loopy"); ESP skips that generate step. */
+#define UMURMUR_CODENAME "Loopy"
 
 #endif /* CONFIG_H */
